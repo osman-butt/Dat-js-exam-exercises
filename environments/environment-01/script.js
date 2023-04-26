0a4bdbd4c5aad765252f4b723465ef4a5be59649
osman-butt <butt.osman@gmail.com>
2023-04-26 11:52:20 +0200

    ex1 done

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,38 @@
 "use strict";
 
+window.addEventListener("load", initApp);
+
+async function initApp() {
+  const users = await getUser();
+  //   users.forEach(showUser);
+  const admin = getAdmin(users);
+  //   admin.forEach(showUser);
+  showUsers(admin);
+}
+
+async function getUser() {
+  const res = await fetch("users.json");
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+function showUser(user) {
+  const html = /*html*/ `
+    <li>${user.name} - active: ${user.active}</li>
+    `;
+
+  document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+}
+
+function showUsers(admin) {
+  admin.forEach(showUser);
+}
+
+function getAdmin(users) {
+  const admins = users.filter(checkRole);
+  return admins;
+  function checkRole(user) {
+    return user.role === "admin";
+  }
+}
